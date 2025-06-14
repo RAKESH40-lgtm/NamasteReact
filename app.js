82ba2a1cdcd4a641d5f1a3f6b7072511aa8f3687
RAKESH40-lgtm <ram19042k@protonmail.com>
2025-06-14 17:36:59 +0530

    layed foundation

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// it's from core react.createElement
 let heading = document.createElement("h1")
 heading.textContent = "Hello from js"
 document.getElementById("root").appendChild(heading)
 
-let react_heading = React.createElement("h1", { id: "heading" }, "Hello from React")
-let root = ReactDOM.createRoot(document.getElementById("root"))
+let react_heading = React.createElement("h1", { id: "heading" }, "Hello from React");
 
 let parent_element = React.createElement("div", {
     id: "parent"
@@ -29,4 +31,25 @@ let parent_element = React.createElement("div", {
         )
     ],
 )
-root.render(parent_element)
+let root = ReactDOM.createRoot(document.getElementById("root"));
+// It's by JSX =>React.createElement() by babel =>ReactElement=js object=>HTML by render from ReactDOM
+
+// single line jsx
+// const jsxHeading=<h1 id="heading">Hello by jsx</h1>
+
+// multi line jsx converted the React element to React component by capitalize and use functional component .
+const JsxHeading = () => (
+    <h1 id="heading">Hello by jsx</h1>
+)
+// component composition adding 1 component with another component .
+const expres="Hello show expression by  {}"
+const FunctionalCompo = () => {
+    return (
+        <div>
+            {expres}
+            <JsxHeading />
+            <h1>Hello by Functional components</h1>
+        </div>
+    )
+}
+root.render(<FunctionalCompo/>)
